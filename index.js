05920a3416472fa1cb77227eb5678f309fd9b264
preston-56 <prestonosoro56@gmail.com>
2022-11-16 17:08:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+// basic math functions
+
+function add(a, b) {
+    return a + b;
+ 
+}
+
+function subtract(a, b) {
+    return a - b;
+    
+}
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function divide(a, b) {
+    return a / b;
+
+}
+
+// Increment function
+function increment(n) {
+    return n += 1;
+
+}
+
+// Decrement function
+function decrement(n) {
+    return n -= 1;
+
+}
+
+// makeInt(string)
+function makeInt(n) {
+    return parseFloat(n);
+
+}
+// preserveDecimal(string)
+function preserveDecimal(n) {
+    return parseFloat(n);
+
+}
